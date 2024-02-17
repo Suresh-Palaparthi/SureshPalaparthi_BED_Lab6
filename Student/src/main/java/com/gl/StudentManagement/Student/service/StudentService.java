f4e8f888c573b58741e7a523c4bda3407722319c
Suresh Palaparthi <91492334+Suresh-Palaparthi@users.noreply.github.com>
2024-02-17 15:03:14 +0530

    Create StudentService.java

---
+++ Student/src/main/java/com/gl/StudentManagement/Student/service/StudentService.java
@@ -0,0 +1,18 @@
+package com.gl.StudentManagement.Student.service;
+
+import java.util.List;
+
+import com.gl.StudentManagement.Student.Model.Student;
+
+public interface StudentService {
+	public List<Student> findAll();
+
+	public Student findById(int theId);
+
+	public void save(Student thestudent);
+
+	public void deleteById(int theId);
+
+	
+
+}
